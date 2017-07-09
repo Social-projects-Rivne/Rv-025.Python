abbb54bbb4ccba48cd5f7f4663a567f3123db967
Olha <olhalesk@gmail.com>
2017-07-10 01:43:14 +0300

    Application Admin is renamed into Administrator (because of this error: Application labels aren't unique, duplicates: admin)

--- administrator/apps.py
+++ administrator/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
-    name = 'admin'
+class AdministratorConfig(AppConfig):
+    name = 'administrator'
