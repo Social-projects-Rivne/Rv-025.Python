b0e4e53c3b10871c399ade2da834b9c01a135ce2
Olha <olhalesk@gmail.com>
2017-07-11 16:27:16 +0300

    Add UserManager.
    UserManager allows us to create users and superusers.

--- administrator/models/user_profile.py
+++ administrator/models/user_profile.py
@@ -9,9 +9,37 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import UserManager
 from django.core.mail import send_mail
 from django.db import models
-from django.db.models.manager import EmptyManager
 
 from django.utils.translation import ugettext_lazy as _
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, name, password, **extra_fields):
+        """Creates and saves a User with the given email, name and password."""
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, name, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, name, password, **extra_fields)
+
+    def create_superuser(self, email, name, password, **extra_fields):
+        """Creates and saves a superuser with the given email, name and password."""
+
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, name, password, **extra_fields)
+
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
@@ -21,7 +49,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         permissions.
 
         Email and password are required. Other fields are optional.
-        """
+    """
 
     ACTIVE = 0
     DELETED = 1
@@ -34,34 +62,29 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         (BANNED, 'banned'),
         (UNAUTHORIZED, 'unauthorized'),
     )
-    first_name = models.CharField(_('first name'), max_length=30, default='name')
-    last_name = models.CharField(_('last name'), max_length=30, default='surname')
+    name = models.CharField(max_length=50)
     email = models.EmailField(_('email address'), unique=True, default='mail',
                               error_messages={
                                   'unique': _("A user with such email already exists."), })
     password = models.CharField(max_length=128, default='pass')
-    phone = models.ImageField(default=0, unique=True)
-    avatar = models.ImageField(upload_to='user_images', blank=True, default='')
+    phone = models.CharField(max_length=12, null=True, unique=True)
+    avatar = models.ImageField(upload_to='user_images', blank=True, null=True)
     status = models.IntegerField(choices=USER_STATUSES, default='3')
+    is_staff = models.BooleanField(default=False, null=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name']
 
     class Meta():
         """This class gives some options (metadata) attached to the model."""
         db_table = "users"
         verbose_name = ('user')
 
-    def get_full_name(self):
-        """Returns the first_name and the last_name with the space in between"""
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
-
     def get_short_name(self):
-        """Returns the first_name of the user"""
-        return self.first_name
+        """Returns the name of the user"""
+        return self.name
 
     def email_to_user(self, subject, message, sender=None, **kwargs):
         """Sends an email to the user"""
