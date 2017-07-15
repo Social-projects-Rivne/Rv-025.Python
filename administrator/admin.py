27d244bf3ea04c040296c89c6cd9a757f10041f7
Olha <olhalesk@gmail.com>
2017-07-15 18:43:53 +0300

    Add blank lines

--- administrator/admin.py
+++ administrator/admin.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 
 from .models import User
 
+
 class UserAdmin(admin.ModelAdmin):
     """Represent a model in the admin interface."""
     fields = ['username', 'email', 'password', 'phone', 'is_staff']
+
 
 # Register your models.
 admin.site.register(User, UserAdmin)
