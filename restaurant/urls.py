fab8f0e2edc2472e55b88e1dcb2b7e5cf658d8d6
Vira Buniak <virynka.b@gmail.com>
2017-07-18 00:56:05 +0300

    Fix indentation

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -20,12 +20,12 @@ from views.index_view import index
 from views.login_view import login_view
 
 urlpatterns = [
-               url(r'^admin/', admin.site.urls),
-               url(r'^index/', index, name='index'),
-               url(r'^account/login/$', login_view, name='login'),
-               url(r'^account/password_reset/$', auth_views.password_reset,
-                   name='password_reset'),
-               url(r'^account/password_reset/done/$',
-                   auth_views.password_reset_done,
-                   name='password_reset_done'),
+   url(r'^admin/', admin.site.urls),
+   url(r'^index/', index, name='index'),
+   url(r'^account/login/$', login_view, name='login'),
+   url(r'^account/password_reset/$', auth_views.password_reset,
+       name='password_reset'),
+   url(r'^account/password_reset/done/$',
+       auth_views.password_reset_done,
+       name='password_reset_done'),
 ]
