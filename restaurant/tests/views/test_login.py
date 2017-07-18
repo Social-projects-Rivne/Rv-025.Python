c540af4e179f9f0df32da6f9635625333a4255d2
Vira Buniak <virynka.b@gmail.com>
2017-07-18 23:46:16 +0300

    Fix docstrings

--- restaurant/tests/views/test_login.py
+++ restaurant/tests/views/test_login.py
@@ -1,13 +1,13 @@
-
-""" Test login view"""
+"""Test login view"""
 
 from django.test import TestCase, Client
+
 from administrator.models import User
 
 
 class LogInTest(TestCase):
 
-    """Test login view"""
+    """Test login view."""
 
     def setUp(self):
         self.client = Client()
@@ -30,7 +30,7 @@ class LogInTest(TestCase):
         User.objects.create_user(**self.users_credentials["banned_user"])
 
     def test_login_correct_user(self):
-        """Tests whether user can login with correct data """
+        """Test whether user can login with correct data."""
         response = self.client.post('/account/login/',
                                     self.users_credentials["correct_user"],
                                     follow=True
@@ -38,7 +38,7 @@ class LogInTest(TestCase):
         self.assertContains(response, "Logout")
 
     def test_login_banned_user(self):
-        """Tests whether banned user can't login """
+        """Test whether banned user can't login."""
         context = ("To proceed, please login with an account "
                    "that has access.")
         response = self.client.post('/account/login/',
@@ -48,13 +48,11 @@ class LogInTest(TestCase):
         self.assertContains(response, context)
 
     def test_login_unregistered_user(self):
-        """Tests whether user that isn't in database can't login"""
+        """Test whether user that isn't in database can't login."""
         context = "Please try again."
         response = self.client.post('/account/login/',
                                     self.users_credentials["unregistered_user"],
                                     follow=True
         )
         self.assertContains(response, context)
-
-
 
