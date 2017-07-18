0d7d7bdee69886d3b3c007e90b78968eee3b3d04
yurapa <yura.paray@gmail.com>
2017-07-18 21:55:28 +0300

    flake8 config, commit non pep8 code

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-# import ConfigParser
+import ConfigParser  # nopep8
 import os
 
 import dj_database_url
@@ -141,6 +141,6 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 try:
-    from .local_settings import *
+    from .local_settings import *  # nopep8
 except ImportError:
     pass
