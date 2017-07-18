fc03f815fa728b9f85ce7494187fac94804a7283
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-07-19 01:08:27 +0300

    Add filters to User model in admin dashboard

--- administrator/admin.py
+++ administrator/admin.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as Admin
 
-# Register your models here.
+from .filters import ChoiceDropdownFilter
+from .models import User
+
+
+class UserAdmin(Admin):
+
+    """Represent a model in the admin interface."""
+
+    list_display = ('username', 'email', 'phone', 'role', 'status')
+    list_filter = [('status', ChoiceDropdownFilter), 'role']
+
+admin.site.register(User, UserAdmin)
