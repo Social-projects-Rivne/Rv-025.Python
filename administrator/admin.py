dbdc16581ce2af3783e47f4e7fb11454eb65e80a
Serhii Kryzhanovskiy <skrizik@mbp.local>
2017-07-18 13:31:05 +0300

    remove useless migrations from commit

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.contrib import admin
+
 from restaurant.models import Restaurant
+
 
 def soft_delete(modeladmin, request, queryset):
     """Soft delete function for selected restaurants in QuerySet list"""
