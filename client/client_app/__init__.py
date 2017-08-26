65fefddc2dbfe7a67d41df9fe2e8483fa0b85def
Vira Buniak <virynka.b@gmail.com>
2017-08-26 16:14:18 +0300

    Add user's model

--- client/client_app/__init__.py
+++ client/client_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,5 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 Bootstrap(app)
 db = SQLAlchemy()
+bcrypt = Bcrypt(app)
+
 
 from client_app.views import index
