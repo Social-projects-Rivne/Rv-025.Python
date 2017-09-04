65a8125b9d55591d993e6809a5d688c65660de2a
Serhii Kryzhanovskiy <skrizik@mbp.local>
2017-09-04 12:45:59 +0300

    add wtf, fix redirect urls and pep8 issues

--- client/client_app/views/index.py
+++ client/client_app/views/index.py
@@ -1,16 +1,29 @@
 from client_app import app
 
-from flask import Flask
 from flask import flash, render_template, redirect, url_for, session, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
 
 from client_app.models.restaurant import Restaurant
 from client_app.models.user import User
 
 
+class LoginForm(FlaskForm):
+    email = StringField(
+        'email',
+        validators=[InputRequired(), Email('Invalid Email')]
+    )
+    password = PasswordField(
+        'password',
+        validators=[InputRequired(), Length(min=8)]
+    )
+
+
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    return render_template('index.html', form=form)
 
 
 @app.route('/restaurant')
@@ -35,7 +48,7 @@ def profile():
 
     user_id = session['logged_in']
     current_user = User.query.get(user_id)
-    return render_template('profile.html', current_user=current_user )
+    return render_template('profile.html', current_user=current_user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,27 +63,25 @@ def do_admin_login():
 
     if request.method == 'POST':
         login_email = str(request.form['email'])
-        login_password = str(request.form['password'])
-        logged_user = User.query.filter_by(email = login_email).first()
+        logged_user = User.query.filter_by(email=login_email).first()
 
-        if logged_user\
-            and logged_user.role == ROLE_USER\
-            and logged_user.status != STATUS_BANNED :
-                session['logged_in'] = logged_user.id
-                session['name'] = logged_user.name
-                redirect('/profile')
-
+        if (logged_user
+                and logged_user.role == ROLE_USER
+                and logged_user.status != STATUS_BANNED):
+            session['logged_in'] = logged_user.id
+            session['name'] = logged_user.name
+            redirect(url_for('profile'))
         elif logged_user and logged_user.status == STATUS_BANNED:
             flash('your account has been banned', 'danger')
             return logout()
         else:
-            flash('wrong login/password!','danger')
+            flash('wrong login/password!', 'danger')
     else:
-        return redirect('/')
-    return redirect('/')
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return redirect('/')
+    return redirect(url_for('index'))
