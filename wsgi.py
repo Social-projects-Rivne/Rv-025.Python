8a93a275ff9d5477c1f3d7a2025145752ec7bc5d
yurapa <yura.paray@gmail.com>
2017-09-14 12:18:17 +0300

    test env var 2

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
+# from backend/restaurant import wsgi as _my_example_1_wsgi
+# import my_example_2_wsgi as _my_example_2_wsgi
+
+
 def application(environ, start_response):
     host = environ.get('HTTP_HOST')
     pathinfo = environ.get('PATH_INFO', '')
     uri = environ.get('REQUEST_URI')
     start_response('200 OK', [('Content-Type', 'text/html')])
-    print host, uri, pathinfo
-    return client/run.py(environ, start_response)
+    print "host: %s, uri: %s, pathinfo: %s" % (host, uri, pathinfo)
+    return client.run(environ, start_response)
