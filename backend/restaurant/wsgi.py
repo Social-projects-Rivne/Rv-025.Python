e6a30863a93a9b946e32cd8da1aba98519f9bff9
yurapa <yura.paray@gmail.com>
2017-09-14 13:08:54 +0300

    add django settings

--- backend/restaurant/wsgi.py
+++ backend/restaurant/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.restaurant.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
