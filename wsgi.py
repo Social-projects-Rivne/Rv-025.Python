8ff4b84b8fbdcf1b371479851f4958b0171695fd
yurapa <yura.paray@gmail.com>
2017-09-14 13:45:54 +0300

    TEST 8

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,15 @@
-import backend.restaurant.wsgi as _my_example_1_wsgi
-import client.run as _my_example_2_wsgi
+import backend.restaurant.wsgi as _backend_wsgi
+import client.run as _client_wsgi
 
 
 def application(environ, start_response):
 
     pathinfo = environ.get('PATH_INFO', '')
-    # selected_application = _my_example_2_wsgi.application
-    # start_response('200 OK', [('Content-Type', 'text/html')])
     print "Get Pathinfo: %s !!!" % pathinfo
 
     if pathinfo == "/admin":
-        selected_application = _my_example_1_wsgi.application
+        selected_application = _backend_wsgi.application
     else:
-        selected_application = _my_example_2_wsgi.app
+        selected_application = _client_wsgi.app
 
     return selected_application(environ, start_response)
