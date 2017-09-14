7b90b84f7f5ee621b1b63fcbc414468892aa7949
yurapa <yura.paray@gmail.com>
2017-09-14 13:31:26 +0300

    add django settings 3

--- backend/restaurant/settings.py
+++ backend/restaurant/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.administrator',
+    'administrator',
     'restaurant',
 ]
 
