535eaef99a99da9249b0d72c7b207614800ad80f
yurapa <yura.paray@gmail.com>
2017-09-14 13:02:49 +0300

    test env var 6

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
-import restaurant.wsgi as _my_example_1_wsgi
+import backend.restaurant.wsgi as _my_example_1_wsgi
 import client.run as _my_example_2_wsgi
 
 
 def application(environ, start_response):
 
     pathinfo = environ.get('PATH_INFO', '')
-    selected_application = _my_example_2_wsgi.application
+    # selected_application = _my_example_2_wsgi.application
     # start_response('200 OK', [('Content-Type', 'text/html')])
-    print "\tGet Pathinfo: %s !!!\n" % pathinfo
+    print "Get Pathinfo: %s !!!" % pathinfo
 
     if pathinfo == "/admin":
         selected_application = _my_example_1_wsgi.application
