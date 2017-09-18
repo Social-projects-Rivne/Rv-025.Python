c8d1b34b8a0554a83ca5212e64dbdf8a57b6f224
yurapa <yura.paray@gmail.com>
2017-09-18 10:39:20 +0300

    Flask: Jinja - rewrite filter groupby, remove default sorting

--- client/client_app/__init__.py
+++ client/client_app/__init__.py
@@ -2,10 +2,25 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+from jinja2 import environmentfilter
+from itertools import groupby
+from jinja2.filters import make_attrgetter, _GroupTuple
+
 
 app = Flask(__name__)
 Bootstrap(app)
 db = SQLAlchemy()
+
+
+# Custom Filter
+# http://jinja.pocoo.org/docs/dev/api/#custom-filters
+@environmentfilter
+def do_not_sorted_groupby(environment, value, attribute):
+    expr = make_attrgetter(environment, attribute)
+    return [_GroupTuple(key, list(values)) for key, values
+            in groupby(value, expr)]
+
+app.jinja_env.filters['not_sorted_groupby'] = do_not_sorted_groupby
 
 
 from client_app.views import index
