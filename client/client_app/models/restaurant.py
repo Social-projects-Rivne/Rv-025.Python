fd3a54640c2017e39c576ddff7480cae38e726c8
yurapa <yura.paray@gmail.com>
2017-09-19 09:11:38 +0300

    add restaurant logo

--- client/client_app/models/restaurant.py
+++ client/client_app/models/restaurant.py
@@ -17,7 +17,7 @@ class Restaurant(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
-    logo = db.Column(db.String(256), default="Logo_added")
+    logo = db.Column(db.String(256), nullable=True)
     location = db.Column(db.String(256))
     phone = db.Column(db.String(256))
     restaurant_type_id = db.Column(
